cc9e92b25192a8f049e37c7eeaa0e6c74f6a7ae4
Alberto Lazari <alberto.lazari@gmail.com>
2023-01-11 19:46:04 +0100

    Test with backtrace

--- test.sh
+++ test.sh
@@ -7,10 +7,8 @@ do
 		echo $(basename $ex) >> $out
 		echo '======================================================' >> $out
 
-		${1:-python3} react_mc_${out/.out/}.py $ex | grep -v 'Counterexample' >> $out
+		${1:-python3} react_mc_${out/.out/}.py $ex >> $out
 
 		echo '' >> $out
 	done
 done
-
-[[ $(diff -u our.out original.out) ]] && echo No || echo Ok
